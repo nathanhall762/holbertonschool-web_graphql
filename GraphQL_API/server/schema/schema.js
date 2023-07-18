d03a83006bb1c57f4f7a9b23c5c34f42c6c7bbba
nathanhall762 <nathanhall762@gmail.com>
2023-07-18 10:24:22 -0500

    push into correct repo

---
+++ GraphQL_API/server/schema/schema.js
@@ -0,0 +1,98 @@
+const { GraphQLID, GraphQLString, GraphQLInt, GraphQLObjectType, GraphQLSchema, GraphQLList } = require('graphql');
+const _ = require('lodash');
+
+const tasks = [
+    {
+        id: '1',
+        projectId: '1',
+        title: 'Create your first webpage',
+        weight: 1,
+        description:
+            'Create your first HTML file 0-index.html with:\n- Add the doctype on the first line (without any comment)\n- After the doctype, open and close an html tag\n- Open your file in your browser (the page should be blank)'
+    },
+    {
+        id: '2',
+        projectId: '1',
+        title: 'Structure your webpage',
+        weight: 1,
+        description:
+            'Copy the content of 0-index.html into 1-index.html\n- Create the head and body sections inside the html tag\n- Create the head and body tags (empty) in this order'
+    }
+];
+
+const projects = [
+    {
+        id: '1',
+        title: 'Advanced HTML',
+        weight: 1,
+        description:
+            'Welcome to the Web Stack specialization. The 3 first projects will give you all basics of the Web development: HTML, CSS and Developer tools. In this project, you will learn how to use HTML tags to structure a web page. No CSS, no styling - don’t worry, the final page will be “ugly” it’s normal, it’s not the purpose of this project. Important note: details are important! lowercase vs uppercase / wrong letter… be careful!'
+    },
+    {
+        id: '2',
+        title: 'Bootstrap',
+        weight: 1,
+        description:
+            'Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web development. It contains CSS and JavaScript design templates for typography, forms, buttons, navigation, and other interface components.'
+    }
+];
+
+const TaskType = new GraphQLObjectType({
+    name: 'Task',
+    fields: () => ({
+        id: { type: GraphQLID },
+        title: { type: GraphQLString },
+        weight: { type: GraphQLInt },
+        description: { type: GraphQLString },
+        project: {
+            type: ProjectType,
+            resolve(parent) {
+                return _.find(projects, { id: parent.projectId });
+            }
+        }
+    })
+});
+
+const ProjectType = new GraphQLObjectType({
+    name: 'Project',
+    fields: () => ({
+        id: { type: GraphQLID },
+        title: { type: GraphQLString },
+        weight: { type: GraphQLInt },
+        description: { type: GraphQLString },
+        tasks: {
+            type: new GraphQLList(TaskType),
+            resolve(parent) {
+                return _.filter(tasks, { projectId: parent.id });
+            }
+        }
+    })
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        task: {
+            type: TaskType,
+            args: {
+                id: { type: GraphQLID }
+            },
+            resolve(parent, args) {
+                return _.find(tasks, { id: args.id });
+            }
+        },
+        project: {
+            type: ProjectType,
+            args: {
+                id: { type: GraphQLID }
+            },
+            resolve(parent, args) {
+                return _.find(projects, { id: args.id });
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+});
